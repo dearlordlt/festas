49fbd97d4db88bf96b6cd2d87ab37d061ae41135
ssliteris <ssliteris@deloitte.com>
2016-09-05 19:15:03 +0300

    initial setup

--- app/app.js
+++ app/app.js
@@ -2,13 +2,31 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+    'ui.router',
+    'myApp.view1',
+    'myApp.view2',
+    'myApp.version'
+]).config(['$locationProvider', '$stateProvider', '$urlRouterProvider',
+    function ($locationProvider, $stateProvider, $urlRouterProvider) {
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+        $locationProvider.html5Mode(true);
+
+        $urlRouterProvider.otherwise('/view1');
+
+        var helloState = {
+            name: 'view1',
+            url: '/view1',
+            templateUrl: 'view1/view1.html'
+        };
+
+        var aboutState = {
+            name: 'view2',
+            url: '/view2',
+            templateUrl: 'view2/view2.html'
+        };
+
+        $stateProvider.state(helloState);
+        $stateProvider.state(aboutState);
+
+    }])
+;
