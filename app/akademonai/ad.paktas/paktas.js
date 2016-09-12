ab98f6293ec9672c4fa9146b0989aaf0c30fa710
Nim <nim@nim.lt>
2016-09-12 20:40:36 +0300

    html and css for paktas

---
+++ app/akademonai/ad.paktas/paktas.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('SatanApp.paktas', [])
+	
+	.controller('PaktasController', ['$scope', function($scope) {
+		
+		$scope.paktas = {
+			lytis: 'abominacija'
+			
+		};
+		//$scope.paktas.lytis = 'abominacija';
+		
+		/*var registration = new Registration({
+			name: req.body.vardas,
+			username: req.body.lytis,
+			password: req.body.titulas,
+			admin: req.body.atvykstu,
+			location: req.body.kunas,
+			meta: req.body.apigosiki,
+			created_at: req.body.pasiulymas
+		});*/
+		
+		
+	}]);
--- app/app.js
+++ app/app.js
@@ -4,11 +4,14 @@ angular.module('SatanApp', [
 
     //External satan stuff
     'ui.router',
-
+    'ui.bootstrap',
+    
     //Internal satan stuff
     'SatanApp.okultas',
     'SatanApp.sventove',
+    'SatanApp.paktas',
     'SatanApp.main'
+    
 
 ]).config(['$locationProvider', '$stateProvider', '$urlRouterProvider',
     function ($locationProvider, $stateProvider, $urlRouterProvider) {
@@ -19,13 +22,22 @@ angular.module('SatanApp', [
         var okultasState = {
             name: 'okultas',
             url: '/',
-            templateUrl: 'akademonai/ad.okultas/okultas.html'
+            templateUrl: 'akademonai/ad.okultas/okultas.html',
+            controller: 'OkultasController'
         };
 
         var sventoveState = {
             name: 'sventove',
             url: '/sventove/',
-            templateUrl: 'akademonai/ad.sventove/sventove.html'
+            templateUrl: 'akademonai/ad.sventove/sventove.html',
+            controller: 'SventoveController'
+        };
+        
+        var paktasState = {
+            name: 'paktas',
+            url: '/paktas/',
+            templateUrl: 'akademonai/ad.paktas/paktas.html',
+            controller: 'PaktasController'
         };
 
         /**
@@ -33,6 +45,7 @@ angular.module('SatanApp', [
          */
         $stateProvider.state(okultasState);
         $stateProvider.state(sventoveState);
+        $stateProvider.state(paktasState);
 
         /*$urlRouterProvider.otherwise(function ($injector) {
             var $state = $injector.get('$state');
