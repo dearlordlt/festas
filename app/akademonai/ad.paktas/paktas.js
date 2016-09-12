5007de8d4a08c07b7f2c2315748d6fb45a82857c
Nim <nim@nim.lt>
2016-09-12 23:04:29 +0300

    js completed for form

--- app/akademonai/ad.paktas/paktas.js
+++ app/akademonai/ad.paktas/paktas.js
@@ -18,33 +18,48 @@ angular.module('SatanApp.paktas', [])
 		
 		$scope.gautiTitula = function () {
 			
-			$scope.paktas.titulas = paktasVardaiFactory.getTitle('istvirkelis', 'Pranas');
+			$scope.paktas.titulas = paktasVardaiFactory.getTitle($scope.paktas.lytis, $scope.paktas.vardas);
 		};
 		
-		console.log(paktasVardaiFactory.getTitle('istvirkelis', 'Pranas'));
+		$scope.validateTitulas = function () {
+			//console.log($scope.paktas.vardas.length > 2 , $scope.paktas.pastas , $scope.paktas.pastas == $scope.paktas.pastas2);
+			return (($scope.paktas.vardas.length > 2) && $scope.paktas.pastas && $scope.paktas.pastas == $scope.paktas.pastas2)
+		};
+		
+		$scope.validatePoreikiai = function () {
+			//console.log($scope.paktas.atvykstu.length > 0 , $scope.paktas.kunas.length > 0 , $scope.paktas.apigosiki.length > 0);
+			return ($scope.paktas.atvykstu.length > 0 && $scope.paktas.kunas.length > 0 && $scope.paktas.apigosiki.length > 0)
+			
+		};
+		
+		/*console.log(paktasVardaiFactory.getTitle('istvirkelis', 'Pranas'));
 		console.log(paktasVardaiFactory.getTitle('istvirkele', 'Gertrūda'));
 		console.log(paktasVardaiFactory.getTitle('abominacija', 'Leonidas'));
+		*/
+		
+		$scope.registered = false;
 		
 		$scope.registerAcademon = function () {
 			
 			var academon = {
-				vardas: 'First',
-				pastas: 'first@first.lt',
-				lytis: 'first',
-				titulas: 'first',
-				atvykstu: 'first',
-				kunas: 'first',
-				apigosiki: 'first',
-				pasiulymas: 'first'
+				vardas: $scope.paktas.vardas,
+				pastas: $scope.paktas.pastas,
+				lytis: $scope.paktas.lytis,
+				titulas: $scope.paktas.titulas,
+				atvykstu: $scope.paktas.atvykstu,
+				kunas: $scope.paktas.kunas,
+				apigosiki: $scope.paktas.apigosiki,
+				pasiulymas: $scope.paktas.pasiulymas
 			};
 			
 			paktasFactory.insertAkademons(academon).then(function(response){
 				
-				
+				$scope.registered = true;
+				$scope.error = null;
 				
 			}, function (error) {
-				
-				
+				$scope.registered = true;
+				$scope.error = error;
 				
 			})
 			
